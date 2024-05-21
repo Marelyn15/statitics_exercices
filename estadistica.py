a98c3f626569b4c6f6b0130e49bc4f8b5204095c
Marelyn Deschamps <78377414+Marelyn15@users.noreply.github.com>
2024-05-20 21:51:42 -0400

    Update estadistica.py
    I recently finished my statistics course, so they are my finished exercises

--- estadistica.py
+++ estadistica.py
@@ -1,48 +1,73 @@
 import statistics as st
+import math
 
-grupo_control = [7,3,-4,14,2,5,22,-7,9,5]
-
-grupo_tratamiento = [-6,5,9,4,4,12,37,5,3,3]
+grupo_control = [3.4,2.5,4.8,2.8,3.3,5.6,4.4,4.0,5.2,2.9,3.7,3.0,3.6,2.8,4.8]
+group = [7, 3, -4, 14, 2, 5, 22, -7, 9, 5]
+group2 = [-6, 5, 9, 4, 4, 12, 37, 5, 3, 3]
 
 def estadisticas(grupo):
-    media = st.mean(grupo)
-    mediana = st.median(grupo)
+    #----------Funciones estadisticas ---------------------------
+    media = round(st.mean(grupo),2)
+    mediana = round(st.median(grupo),2)
+    tamanio = len(grupo)
 
-    def media_recortada(lista, porcentaje):
+    #--------------Funciones hechas ----------------------------------
+    def media_acotada_dos_lados(lista, porcentaje):
         lista = sorted(lista)
-        print(lista, porcentaje)
         porcentaje = porcentaje / 100
         digitos_en_lista = len(lista)
         numeros_quitar = digitos_en_lista * porcentaje
-        print(numeros_quitar)
         i = 0
         while i < numeros_quitar:
             del lista[0]
-            print(lista)
             del lista[-1]
-            print(lista)
             i = i + 1
-            print(i)
         return(lista)
     
     def media_acotada(lista, porcentaje):
         lista = sorted(lista)
-        print(lista, porcentaje)
         porcentaje = porcentaje / 100
         digitos_en_lista = len(lista)
         numeros_quitar = digitos_en_lista * porcentaje
-        print(numeros_quitar)
         i = 0
         while i < numeros_quitar:
             del lista[0]
-            print(lista)
             i = i + 1
-            print(i)
         return(lista)
     
-    media_ya_recortada = st.mean(media_acotada(grupo, 10))
-    media_ya_acotada = st.mean(media_recortada(grupo, 10))
-    print(f"Estos son los resultados del {grupo}: la media es: {media}, la mediana es: {mediana}, la media recortada es:  {media_ya_recortada}, y la media recortada es:  {media_ya_acotada}")
+    def varianza(lista):
+        largo = tamanio - 1
+        all_values = []
+        for i in lista:
+            element = (i - media) ** 2
+            all_values.append(element)
+        all_values = sum(all_values)
+        resultado = 1 / largo * (all_values)
+        return resultado
+    
+    def estandar(lista):
+        largo = tamanio - 1
+        all_values = []
+        for i in lista:
+            element = (i - media) ** 2
+            all_values.append(element)
+        all_values = sum(all_values)
+        resultado = 1 / largo * (all_values)
+        return round(math.sqrt(resultado),2)
 
-estadisticas(grupo_control)
-estadisticas(grupo_tratamiento)
+
+
+    #-----------------------resultados-----------------------------------------------------------------
+    calculo_varianza = round(varianza(grupo),2)
+    desviancion_estandar = estandar(grupo)
+    media_ya_acotada_dos_lados = round(st.mean(media_acotada_dos_lados(grupo, 10)),2)
+    media_ya_acotada = round(st.mean(media_acotada(grupo, 10)),2)
+
+
+    #---------------------resultado final--------------------------------------------------------------
+    print(f"Estos son los resultados del {grupo}: el tamaño es: {tamanio} la media es: {media}, la mediana es: {mediana}, la media acortada es a dos lados:  {media_ya_acotada_dos_lados}, la media acortada es: {media_ya_acotada}, la varianza es {calculo_varianza}, y la desviación estándar es {desviancion_estandar}")
+    
+#estadisticas(grupo_control)
+estadisticas(group)
+estadisticas(group2)
+#estadisticas(grupo_tratamiento)
